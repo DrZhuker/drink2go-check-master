7049e1342f9054c2cfb43375302c51a72c5fd37c
DrZhuker <drzhuker@gmail.com>
2024-10-16 00:18:39 +0300

    add header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,26 @@
-/* в этот файл добавляет скрипты*/
+
+const menuLink = document.querySelector('.user-menu__link--menu');
+const headerNavigation = document.querySelector('.navigation');
+
+if (menuLink && headerNavigation) {
+  menuLink.addEventListener('click', () => {
+    headerNavigation.classList.toggle('navigation--close');
+    menuLink.classList.toggle('user-menu__link--menu-opened');
+  });
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      if (!headerNavigation.classList.contains('navigation--close')) {
+        headerNavigation.classList.add('navigation--close');
+        menuLink.classList.remove('user-menu__link--menu-opened');
+      }
+    }
+  });
+  document.addEventListener('click', (event) => {
+    if (!menuLink.contains(event.target) && !headerNavigation.contains(event.target)) {
+      if (!headerNavigation.classList.contains('navigation--close')) {
+        headerNavigation.classList.add('navigation--close');
+        menuLink.classList.remove('user-menu__link--menu-opened');
+      }
+    }
+  });
+}
